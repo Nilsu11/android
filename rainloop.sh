f4f3200c0c6d57e541ea6bc273a20420c2dccd9e
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-11 15:54:19 +0200

    Update rainloop.sh

--- rainloop.sh
+++ rainloop.sh
@@ -3,5 +3,19 @@ mkdir -p ~/bin
 mkdir -p ~/android/lineage
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
-cd ~/android
+cd ~/android/lineage
 git clone https://github.com/LineageOS/android.git
+echo '<?xml version="1.0" encoding="UTF-8"?>' >> r.xml
+echo '<manifest>' >> r.xml
+echo '  <include name="android/default.xml" />' >> r.xml
+echo '  <include name="packages.xml" />' >> r.xml
+echo '  <repo-hooks in-project="platform/tools/repohooks" enabled-list="pre-upload" />' >> r.xml
+echo '</manifest>' >> r.xml
+echo '<?xml version="1.0" encoding="UTF-8"?>' >> packages.xml
+echo '<manifest>' >> packages.xml
+echo '  <remote  name="omnirom"' >> packages.xml
+echo '           fetch="https://github.com/omnirom"' >> packages.xml
+echo '           review="gerrit.omnirom.org" />' >> packages.xml
+echo '  <remove-project name="platform/packages/apps/DeskClock" />' >> packages.xml
+echo '  <project path="packages/apps/OmniClock" name="android_packages_apps_OmniClock" remote="omnirom" revision="android-10" />' >> packages.xml
+echo '</manifest>' >> packages.xml
